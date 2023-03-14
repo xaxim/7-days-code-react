077c51fe2b1f7852869df73e28a08d60da90a80e
Carlos Lima <carloslima@mpdft.mp.br>
2023-03-14 00:13:31 -0300

    Depois de ver a cola do dia 1

--- src/components/AssinaturaNewsletter/index.tsx
+++ src/components/AssinaturaNewsletter/index.tsx
@@ -1,21 +1,33 @@
+import styles from './index.module.css';
+import formStyles from './form.module.css';
+
 const AssinaturaNewsletter = () => {
   return (
-    <section>
-      <h1>
-        Sua casa com as
-        <br />
-        <strong>
-          melhores <br /> plantas
-        </strong>
-      </h1>
-      <p>
-        Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.
-      </p>
-      <form>
-        <input type="email" placeholder="Digite seu e-mail" />
-        <button>Assinar Newsletter</button>
-      </form>
-    </section>
+    <div className={styles.assinaturaNewsletter}>
+      <div className={styles.assinaturaNewsletter__container}>
+        <div className={styles.assinaturaNewsletter__container__text}>
+          <h1>
+            Sua casa com as
+            <br />
+            <strong>
+              melhores <br /> plantas
+            </strong>
+          </h1>
+          <p>
+            Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.
+          </p>
+        </div>
+
+        <form className={formStyles.form} action="/">
+          <div className={formStyles.fieldGroup}>
+            <input type="email" placeholder='Insira seu E-mail' />
+            <button>
+              Assinar Newsletter
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
